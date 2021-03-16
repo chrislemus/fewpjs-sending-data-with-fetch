56e3f90301e80eda8abaeb024628a471b754a5e7
chrislemus <icrislemus@gmail.com>
2021-03-16 11:32:31 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Add your code here
+// let formData = {
+//   dogName: "Byron",
+//   dogBreed: "Poodle"
+// };
+// configurationObject = {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     "Accept": "application/json"
+//   },
+//   body: JSON.stringify({
+//     dogName: "Byron",
+//     dogBreed: "Poodle"
+//   })
+// }; 
+
+// fetch("http://localhost:3000/dogs", configurationObject); 
+
+function submitData(name, email) {
+  const url = 'http://localhost:3000/users';
+  const config = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({ name, email })
+  }
+
+  return fetch(url, config)
+  .then(res =>  res.json())
+  .then(json => document.body.innerHTML += json.id)
+  .catch(err => document.body.innerHTML += err.message)
+
+}
+
